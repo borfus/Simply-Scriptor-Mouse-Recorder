791e73257ed319ab6baea71317e2dc7d761ca3ed
borfus <mrpeteyb@gmail.com>
2018-10-04 04:11:06 -0500

    Added main file

---
+++ mouse_recorder.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    POINT cursor;
+    bool pressed = false;
+
+    for (;;)
+    {
+        GetCursorPos(&cursor);
+
+        if (GetKeyState(VK_F2) & 0x8000)
+        {
+            if (!pressed)
+            {
+                printf("move_mouse(%i, %i)\n", cursor.x, cursor.y);
+                pressed = true;
+            }
+        }
+        else if (GetKeyState(VK_F3) & 0x8000)
+        {
+            if (!pressed)
+            {
+                printf("left_click()\n");
+                pressed = true;
+            }
+        }
+        else if (GetKeyState(VK_ESCAPE) & 0x8000)
+        {
+            break;
+        }
+        else
+        {
+            pressed = false;
+        }
+    }
+
+    return 0;
+}
